e1728289260db2ae145b986886ae9e7baf20df8d
ktatiana11 <ktastudillo@soy.sena.edu.co>
2024-06-25 10:34:09 -0500

    Agregue la funcion para consignar dinero y sigo con la de consultar

--- ejercicio1.php
+++ ejercicio1.php
@@ -1,4 +1,16 @@
 <?php
-
-
+// Función para consignar dinero
+function consignar ($cantidad){
+    if($cantidad>0){
+        $array ['saldo'] += $cantidad;
+        return "Se ha consignado $cantidad";
+    }
+    else{
+        return "La cantidad a consignar debe ser mayor a cero.";
+    }
+}
+// Función para consultar el saldo
+function consultar(){
+    
+}
 ?>
