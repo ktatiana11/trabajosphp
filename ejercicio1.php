ed07b7d1acc79c9f5800e45d27ab365662d860fc
ktatiana11 <ktastudillo@soy.sena.edu.co>
2024-06-25 10:43:25 -0500

    Termine la funcion de consultar dinero y termine la de retirar dinero

--- ejercicio1.php
+++ ejercicio1.php
@@ -11,6 +11,21 @@ function consignar ($cantidad){
 }
 // Función para consultar el saldo
 function consultar(){
-    
+    return "Su saldo actual es de:" .$array ['saldo'];
+}
+
+// Función para retirar dinero
+function retirar ($cantidad){
+    if ($cantidad>0){
+        if ($cantidad <=$array ['saldo'] ){
+            $array ['saldo'] -= $cantidad;
+            return "Ha retirado $cantidad. Su nuevo saldo es: " . $array ['saldo'];
+        }
+        else {
+            return "Saldo insuficiente. Su saldo actual es: " . $array['saldo'];
+        }
+    } else {
+        return "La cantidad a retirar debe ser mayor a cero";
+    }
 }
 ?>
